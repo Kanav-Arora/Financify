6f2e4122638d4f160918591d403ea8e90a0e33f1
Kanav-Arora <kanavarora1515@gmail.com>
2021-05-08 13:50:56 +0530

    Delete confirm added

--- EndSem/src/Frames/Acc_Delete_Confirm.java
+++ EndSem/src/Frames/Acc_Delete_Confirm.java
@@ -5,11 +5,8 @@
  */
 package Frames;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
